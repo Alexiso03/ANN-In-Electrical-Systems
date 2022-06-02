7b8e7ba30352a4bfffa670d0df547f4fcfac9562
Bipin Kumar Singh <86974424+Alexiso03@users.noreply.github.com>
2022-06-02 20:39:52 +0530

    Delete AnnTrain.m

--- AnnTrain.m
+++
@@ -1,8 +0,0 @@
-I = input';
-T = output';
-net=newff(minmax(I),[3,5,1],{'logsig','tansig','purelin'},'trainlm');
-net = init(net);
-net.trainParam.show=1;
-net.trainParam.epochs=10000;
-net.trainParam.goal=1e-12;
-net=train(net,I,T);
